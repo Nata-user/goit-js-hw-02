f3a98fb3b45a98abaf82a6f2d67864a8624c3fce
Nata-user <70835039+Nata-user@users.noreply.github.com>
2020-12-16 13:25:15 +0200

    1

--- js/task4.js
+++ js/task4.js
@@ -1,13 +1,14 @@
 const formatString = function (string) {
-  const result = string.length
+  let result = string.length
   if (result <= 40) {
     string;
-   return string;
-  } 
-  else 
-  string.slice(0, 40).split(' ').join('...');
- return  string.slice(0, 40).split(' ').join('...');
-}
+    return string;
+  }
+  else
+    string.slice(0, 40) + "...";
+  return string.slice(0, 40) + "...";
+};
+
   
 console.log(formatString("Curabitur ligula sapien, tincidunt non."))
 // вернется оригинальная строка
@@ -17,7 +18,5 @@ console.log(formatString("Curabitur ligula sapien."))
 // вернется оригинальная строка
 console.log(formatString("Nunc sed turpis. Curabitur a felis in nunc fringilla tristique."))
 // вернется форматированная строка
-
- 
 
 
